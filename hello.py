582c3e0fd691c1d86f39947e8b20a3a4bfb8a5b2
Rishi Rathi <143132494+rishirathi1@users.noreply.github.com>
2025-04-05 10:23:46 -0700

    rishi is an idiot

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+print("rishi is an idiot")
+print("rishi is an idiot")
+print("rishi is an idiot")
+print("rishi is an idiot")
+print("rishi is an idiot")
+print("rishi is an idiot")
